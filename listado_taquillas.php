42d217a4d3ff7a987bfa0907f57ba2a7b25487dc
Enrique Fdez-Campoamor <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2023-12-20 12:19:19 +0100

    All done, fixed to save session atributes

---
+++ listado_taquillas.php
@@ -0,0 +1,79 @@
+<?php
+require "connection.php";
+$conexion = conectarBD();
+
+session_start();
+
+if (isset($_GET['localidad'])) {
+    $localidad = $_GET['localidad'];
+    $_SESSION['localidad'] = $localidad;
+} else if (isset($_SESSION['localidad'])) {
+    $localidad = $_SESSION['localidad'];
+} else {
+    $localidad = "todas las localidades";
+}
+
+?>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Taquillator</title>
+</head>
+
+<body>
+    <form action="" method="get">
+    <select name="localidad">
+        <option value="todas las localidades" <?php echo isset($_SESSION['localidad']) && $_SESSION['localidad'] == 'todas las localidades' ? 'selected' : ''; ?>>Todas las localidades</option>
+        <option value="Gijón" <?php echo isset($_SESSION['localidad']) && $_SESSION['localidad'] == 'Gijón' ? 'selected' : ''; ?>>Gijón</option>
+        <option value="Oviedo" <?php echo isset($_SESSION['localidad']) && $_SESSION['localidad'] == 'Oviedo' ? 'selected' : ''; ?>>Oviedo</option>
+        <option value="Avilés" <?php echo isset($_SESSION['localidad']) && $_SESSION['localidad'] == 'Avilés' ? 'selected' : ''; ?>>Avilés</option>
+    </select>
+        <input type="submit" value="Buscar">
+    </form>
+
+</body>
+
+</html>
+
+
+
+<?php
+
+if (isset($_SESSION['localidad'])) {
+    $localidad = $_SESSION['localidad'];
+} else {
+    $localidad = $_GET['localidad'];
+}
+
+////////////////////////////////////////////
+// TODO 2: Obtener taquillas según filtro //
+////////////////////////////////////////////
+
+$sql = "SELECT localidad, direccion, capacidad, ocupadas FROM puntosderecogida WHERE capacidad != ocupadas";
+if($localidad !== "todas las localidades"){
+    $sql .= " AND localidad = :localidad";
+    $stmt = $conexion->prepare($sql);
+    $stmt->bindParam(':localidad', $localidad);
+    $stmt->execute();
+   
+} else {
+    $stmt = $conexion->query($sql);
+}
+
+if ($stmt->rowCount() > 0) {
+    echo "<table><tr><th>Localidad</th><th>Dirección</th><th>Capacidad</th><th>Ocupadas</th></tr>";
+    /////////////////////////////////////
+    // TODO 3: Imprimir filas de tabla //
+    /////////////////////////////////////
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        echo "<tr><td>".$row["localidad"]."</td><td>".$row["direccion"]."</td><td>".$row["capacidad"]."</td><td>".$row["ocupadas"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "No hay resultados";
+}
+
+?>
---
+++ nueva_taquilla.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Añadir Nueva Taquilla</title>
+</head>
+<body>
+    <h2>Añadir Nueva Taquilla</h2>
+    <form action="nueva_taquilla.php" method="post">
+        <label for="localidad">Localidad:</label><br>
+        <select id="localidad" name="localidad" required>
+            <option value="">Seleccione una localidad</option>
+            <option value="Gijón">Gijón</option>
+            <option value="Oviedo">Oviedo</option>
+            <option value="Avilés">Avilés</option>
+        </select><br>
+        
+        <label for="direccion">Dirección:</label><br>
+        <input type="text" id="direccion" name="direccion" required><br>
+        
+        <label for="capacidad">Capacidad:</label><br>
+        <input type="number" id="capacidad" name="capacidad" min="1" required><br>
+        
+        <label for="ocupadas">Taquillas Ocupadas:</label><br>
+        <input type="number" id="ocupadas" name="ocupadas" min="0" required><br>
+        
+        <input type="submit" value="Añadir Taquilla">
+    </form>
+</body>
+</html>
+
+
+<?php
+    require_once 'connection.php';
+    $conexion = conectarBD();
+
+    
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    /////////////////////////////////////////////////
+    // TODO 1:Guardar la info en la base de datos. //
+    /////////////////////////////////////////////////
+
+    $localidad = $_POST["localidad"];
+    $direccion = $_POST["direccion"];
+    $capacidad = $_POST["capacidad"];
+    $ocupadas = $_POST["ocupadas"];
+
+    $sql = "INSERT INTO puntosderecogida (localidad, direccion, capacidad, ocupadas) VALUES (:localidad, :direccion, :capacidad, :ocupadas)";
+    $stmt = $conexion->prepare($sql);
+    $stmt->bindParam(':localidad', $localidad);
+    $stmt->bindParam(':direccion', $direccion);
+    $stmt->bindParam(':capacidad', $capacidad);
+    $stmt->bindParam(':ocupadas', $ocupadas);
+
+    // $stmt->execute();
+
+    if ($stmt->execute()) {
+        echo "Nueva taquilla añadida con éxito.";
+    } else {
+        echo "Error al añadir la taquilla.";
+    }
+}
+?>
